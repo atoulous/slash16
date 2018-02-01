0c38450c026436e9cd518b39a3547c054966a65b
Aymeric TOULOUSE <atoulous@e2r11p13.42.fr>
2018-02-01 14:21:01 +0100

    final

--- scripting/04.sh
+++ scripting/04.sh
@@ -1,4 +1,4 @@
-script='sudo cat /etc/crontab > /home/vagrant/crontab.sum
+script='sudo cat /etc/crontab > /home/crontab.sum
 file=$( md5sum "/home/crontab.sum" | awk "{print $1}")
 file2=$( md5sum "/etc/crontab" | awk "{print $1}")
 if [ "x$file1_md5" = "x$file2_md5" ]
@@ -8,7 +8,7 @@ then
 else
 	echo "Modif"
 	sudo cat /etc/crontab > /home/crontab.sum
-	exit 1
+	exit 0
 fi'
 
 echo " 0 0 * * * $script" >> /etc/crontab; /etc/init.d/cron restart
